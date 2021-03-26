1d515ec12457c5b61df8cf0659900eec219eea27
UnderAnder <andryushin.alexander@gmail.com>
2021-03-26 21:07:26 +0700

    Stage 3/6: Hotkeys

--- browser.py
+++ browser.py
@@ -1,9 +1,7 @@
 from sys import argv
 from os import mkdir, path
-
-
-def main():
-    nytimes_com = '''
+from _collections import deque
+nytimes_com = '''
 This New Liquid Is Magnetic, and Mesmerizing
 
 Scientists have created “soft” magnets that can flow 
@@ -19,7 +17,7 @@ Jessica Wade has added nearly 700 Wikipedia biographies for
 
 '''
 
-    bloomberg_com = '''
+bloomberg_com = '''
 The Space Race: From Apollo 11 to Elon Musk
 
 It's 50 years since the world was gripped by historic images
@@ -37,37 +35,51 @@ Twitter and Square Chief Executive Officer Jack Dorsey
  addressed Apple Inc. employees at the iPhone maker’s headquarters
  Tuesday, a signal of the strong ties between the Silicon Valley giants.
 '''
-    _, target_dir = argv
-
-    if not path.isdir(target_dir):
-        mkdir(target_dir)
 
 
-    while True:
-        raw_input = input()
-        site_name = raw_input.rpartition(".")[0]
+class Browser:
+    def __init__(self):
+        self.history = deque()
 
-        if raw_input == 'exit':
-            exit()
-        elif path.isfile(f'{target_dir}/{site_name}'):
-            with open(f'{target_dir}/{site_name}', 'r') as f:
-                print(f.read())
-        elif raw_input.find('.') == -1:
-            print('error wrong url')
-        elif raw_input == 'bloomberg.com':
-            print(bloomberg_com)
-            write_to_file(bloomberg_com, target_dir, site_name)
-        elif raw_input == 'nytimes.com':
-            print(nytimes_com)
-            write_to_file(nytimes_com, target_dir, site_name)
-        else:
-            print('error')
+    def start(self):
+        target_dir = argv[1]
+
+        if not path.isdir(target_dir):
+            mkdir(target_dir)
+
+        while True:
+            raw_input = input()
+            site_name = raw_input.rpartition(".")[0]
+            file_path = f'{target_dir}/{site_name}'
+            if site_name:
+                self.history.append(site_name)
+            if raw_input == 'exit':
+                exit()
+            elif raw_input == 'back':
+                if len(self.history) > 1:
+                    self.read_file(f'{target_dir}/{self.history[-2]}')
+            elif path.isfile(file_path):
+                self.read_file(file_path)
+            elif raw_input.find('.') == -1:
+                print('error wrong url')
+            elif raw_input == 'bloomberg.com':
+                print(bloomberg_com)
+                self.write_to_file(bloomberg_com, file_path)
+            elif raw_input == 'nytimes.com':
+                print(nytimes_com)
+                self.write_to_file(nytimes_com, file_path)
+            else:
+                print('error')
+
+    def read_file(self, file_path):
+        with open(file_path, 'r') as f:
+            print(f.read())
 
 
-def write_to_file(url, target_dir, site_name):
-    with open(f'{target_dir}/{site_name}', 'w') as f:
-        print(url, file=f)
+    def write_to_file(self, url, file_path):
+        with open(file_path, 'w') as f:
+            print(url, file=f)
 
 
 if __name__ == '__main__':
-    main()
+    Browser().start()
