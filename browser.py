54503daa80247696062059673ce5b70fd408f75a
UnderAnder <andryushin.alexander@gmail.com>
2021-03-26 23:27:52 +0700

    Stage 5/6: Parsing

--- browser.py
+++ browser.py
@@ -2,9 +2,9 @@ from _collections import deque
 from os import mkdir, path
 from re import match
 from sys import argv
+from bs4 import BeautifulSoup
 
 import requests
-
 
 class Browser:
     def __init__(self):
@@ -32,14 +32,14 @@ class Browser:
                 if len(self.history) > 1:
                     self.read_file(f'{target_dir}/{self.history[-2]}')
             elif raw_input.find('.') == -1:
-                self.read_file(file_path) if path.isfile(file_path) else print('error wrong url')
+                self.read_file(file_path) if path.isfile(file_path) else print('Incorrect URL')
             elif match(r'[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)', raw_input):
                 content = self.get_site(raw_input)
                 self.history.append(site_name)
                 print(content)
                 self.write_to_file(content, file_path)
             else:
-                print('error')
+                print('Incorrect URL')
 
     def get_site(self, raw_input: str):
         if not raw_input.startswith('http'):
@@ -47,7 +47,8 @@ class Browser:
         req = requests.get(raw_input, headers={'User-Agent': 'Mozilla/5.0'})
         if not req:
             print(req.status_code, req.reason)
-        return req.text
+        soup = BeautifulSoup(req.content, 'lxml')
+        return soup.get_text()
 
     def read_file(self, file_path):
         with open(file_path, 'r', encoding='utf-8') as f:
