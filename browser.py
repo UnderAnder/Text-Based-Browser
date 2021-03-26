8d41dacf4a798253eb55abbde4e539990523ca60
UnderAnder <andryushin.alexander@gmail.com>
2021-03-26 20:11:04 +0700

    Stage 2/6: Tabs

--- browser.py
+++ browser.py
@@ -1,3 +1,7 @@
+from sys import argv
+from os import mkdir, path
+
+
 def main():
     nytimes_com = '''
 This New Liquid Is Magnetic, and Mesmerizing
@@ -33,17 +37,37 @@ Twitter and Square Chief Executive Officer Jack Dorsey
  addressed Apple Inc. employees at the iPhone maker’s headquarters
  Tuesday, a signal of the strong ties between the Silicon Valley giants.
 '''
+    _, target_dir = argv
+
+    if not path.isdir(target_dir):
+        mkdir(target_dir)
+
 
     while True:
         raw_input = input()
+        site_name = raw_input.rpartition(".")[0]
+
         if raw_input == 'exit':
             exit()
+        elif path.isfile(f'{target_dir}/{site_name}'):
+            with open(f'{target_dir}/{site_name}', 'r') as f:
+                print(f.read())
+        elif raw_input.find('.') == -1:
+            print('error wrong url')
         elif raw_input == 'bloomberg.com':
             print(bloomberg_com)
+            write_to_file(bloomberg_com, target_dir, site_name)
         elif raw_input == 'nytimes.com':
             print(nytimes_com)
+            write_to_file(nytimes_com, target_dir, site_name)
+        else:
+            print('error')
 
 
-# write your code here
+def write_to_file(url, target_dir, site_name):
+    with open(f'{target_dir}/{site_name}', 'w') as f:
+        print(url, file=f)
+
+
 if __name__ == '__main__':
     main()
