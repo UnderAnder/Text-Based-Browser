84263e2337c1adceb7cc8bce1d3865c72b1922a3
UnderAnder <andryushin.alexander@gmail.com>
2021-03-30 17:43:13 +0700

    Stage 6/6: Formatted output

--- browser.py
+++ browser.py
@@ -1,10 +1,13 @@
-from _collections import deque
+import re
+from collections import deque
 from os import mkdir, path
-from re import match
+from re import match, sub
 from sys import argv
+
+import colorama
+import requests
 from bs4 import BeautifulSoup
 
-import requests
 
 class Browser:
     def __init__(self):
@@ -36,8 +39,8 @@ class Browser:
             elif match(r'[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)', raw_input):
                 content = self.get_site(raw_input)
                 self.history.append(site_name)
-                print(content)
                 self.write_to_file(content, file_path)
+                print(self.read_file(file_path))
             else:
                 print('Incorrect URL')
 
@@ -47,8 +50,26 @@ class Browser:
         req = requests.get(raw_input, headers={'User-Agent': 'Mozilla/5.0'})
         if not req:
             print(req.status_code, req.reason)
-        soup = BeautifulSoup(req.content, 'lxml')
-        return soup.get_text()
+        soup = BeautifulSoup(req.content, 'html.parser')
+
+        content = []
+        links_tags = soup.find_all('a')
+        for tag in soup.find_all(["p"]):
+            tag_text = str(tag.text.strip())
+            if tag not in links_tags and tag_text:
+                content.append(tag_text)
+
+        for tag in links_tags:
+            link_text = str(tag.text.strip())
+            if link_text:
+                content.append(colorama.Fore.BLUE + link_text)
+
+        result = []
+        for line in content:
+            line = sub(r'\s', ' ', line)
+            result.append(line.strip())
+        return result
+
 
     def read_file(self, file_path):
         with open(file_path, 'r', encoding='utf-8') as f:
@@ -56,7 +77,7 @@ class Browser:
 
     def write_to_file(self, content, file_path):
         with open(file_path, 'w', encoding='utf-8') as f:
-            f.write(content)
+            print('\n'.join(x for x in content), file=f)
 
 
 if __name__ == '__main__':
